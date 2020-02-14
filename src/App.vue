b902fdb00d3a7698771ab01c994be7ef4a8fac0a
Paul Duvall <paul.duvall@myfuturechoice.com>
2020-02-14 16:47:59 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <h1>{{ title }}</h1>
+    <AllFriends :friends="friends" @delete="deleteFriend" />
+    <OnlineFriends :friends="friends" />
+    <Blogs />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import AllFriends from './AllFriends'
+import OnlineFriends from './OnlineFriends'
+import Blogs from './Blogs'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar: Navbar,
+    AllFriends: AllFriends,
+    OnlineFriends: OnlineFriends,
+    Blogs: Blogs
+  },
+  data () {
+    return {
+      title: 'A new vue app!',
+      friends: [
+        { name: 'Mario', online: true },
+        { name: 'Luigi', online: false },
+        { name: 'Toad', online: true },
+        { name: 'Bowser', online: false },
+      ] 
+    }
+  },
+  methods: {
+    deleteFriend(payload){
+      console.log(payload)
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name
+      })
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
---
+++ src/Blogs.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="blogs">
+        <h2>{{ blogTitle }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Blogs',
+    data() {
+        return {
+            blogTitle: 'Blogs'
+        }
+    },
+    beforeCreate() {
+        alert('beforeCreat hook')
+    },
+    created() {
+        alert('created hook')
+    }
+}
+</script>
